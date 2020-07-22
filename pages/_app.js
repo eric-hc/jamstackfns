61ffeff94315df93ccc5d7b57e0a2a530d811021
Lee Robinson <lrobinson@hy-vee.com>
2020-07-22 13:02:11 -0500

    Add Fathom analytics.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import NextApp from 'next/app';
+import React, { useEffect } from 'react';
 import { MDXProvider } from '@mdx-js/react';
 import { Global, css } from '@emotion/core';
 import { DefaultSeo } from 'next-seo';
 import { ThemeProvider, CSSReset } from '@chakra-ui/core';
+import Router from 'next/router';
+import * as Fathom from 'fathom-client';
 
 import theme from '../styles/theme';
 import { prismDarkTheme } from '../styles/prism';
@@ -39,21 +40,29 @@ const GlobalStyle = ({ children }) => (
   </>
 );
 
-class App extends NextApp {
-  render() {
-    const { Component } = this.props;
+Router.events.on('routeChangeComplete', () => {
+  Fathom.trackPageview();
+});
 
-    return (
-      <ThemeProvider theme={theme}>
-        <MDXProvider components={MDXComponents}>
-          <GlobalStyle>
-            <DefaultSeo {...SEO} />
-            <Component />
-          </GlobalStyle>
-        </MDXProvider>
-      </ThemeProvider>
-    );
-  }
-}
+const App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      Fathom.load('QVYDAVHO', {
+        includedDomains: ['jamstackfns.com']
+      });
+    }
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <MDXProvider components={MDXComponents}>
+        <GlobalStyle>
+          <DefaultSeo {...SEO} />
+          <Component {...pageProps} />
+        </GlobalStyle>
+      </MDXProvider>
+    </ThemeProvider>
+  );
+};
 
 export default App;
